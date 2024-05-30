356f8b09407929aa40488146e38da5fec71befb5
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-05-30 19:36:22 +0200

    Update scanner.go

--- pkg/iac/scanners/kubernetes/scanner.go
+++ pkg/iac/scanners/kubernetes/scanner.go
@@ -108,7 +108,7 @@ func (s *Scanner) Name() string {
 	return "Kubernetes"
 }
 
-func (s *Scanner) initRegoScanner(srcFS fs.FS) (*rego.Scanner, error) {
+func (s *Scanner) initRegoScanner(ctx context.Context, srcFS fs.FS) (*rego.Scanner, error) {
 	s.Lock()
 	defer s.Unlock()
 	if s.regoScanner != nil {
@@ -116,7 +116,7 @@ func (s *Scanner) initRegoScanner(srcFS fs.FS) (*rego.Scanner, error) {
 	}
 	regoScanner := rego.NewScanner(types.SourceKubernetes, s.options...)
 	regoScanner.SetParentDebugLogger(s.debug)
-	if err := regoScanner.LoadPolicies(s.loadEmbeddedLibraries, s.loadEmbeddedPolicies, srcFS, s.policyDirs, s.policyReaders); err != nil {
+	if err := regoScanner.LoadPolicies(ctx, s.loadEmbeddedLibraries, s.loadEmbeddedPolicies, srcFS, s.policyDirs, s.policyReaders); err != nil {
 		return nil, err
 	}
 	s.regoScanner = regoScanner
@@ -160,7 +160,7 @@ func (s *Scanner) ScanFS(ctx context.Context, target fs.FS, dir string) (scan.Re
 		}
 	}
 
-	regoScanner, err := s.initRegoScanner(target)
+	regoScanner, err := s.initRegoScanner(ctx, target)
 	if err != nil {
 		return nil, err
 	}
