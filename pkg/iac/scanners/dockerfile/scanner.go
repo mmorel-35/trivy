51900e77e007b9c774538872f32eda94ea6814e8
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-05-30 19:28:43 +0200

    Update scanner.go

--- pkg/iac/scanners/dockerfile/scanner.go
+++ pkg/iac/scanners/dockerfile/scanner.go
@@ -153,7 +153,7 @@ func (s *Scanner) ScanFile(ctx context.Context, fsys fs.FS, path string) (scan.R
 	})
 }
 
-func (s *Scanner) initRegoScanner(srcFS fs.FS) (*rego.Scanner, error) {
+func (s *Scanner) initRegoScanner(ctx context.Context, srcFS fs.FS) (*rego.Scanner, error) {
 	s.Lock()
 	defer s.Unlock()
 	if s.regoScanner != nil {
@@ -162,7 +162,7 @@ func (s *Scanner) initRegoScanner(srcFS fs.FS) (*rego.Scanner, error) {
 
 	regoScanner := rego.NewScanner(types.SourceDockerfile, s.options...)
 	regoScanner.SetParentDebugLogger(s.debug)
-	if err := regoScanner.LoadPolicies(s.loadEmbeddedLibraries, s.loadEmbeddedPolicies, srcFS, s.policyDirs, s.policyReaders); err != nil {
+	if err := regoScanner.LoadPolicies(ctx, s.loadEmbeddedLibraries, s.loadEmbeddedPolicies, srcFS, s.policyDirs, s.policyReaders); err != nil {
 		return nil, err
 	}
 	s.regoScanner = regoScanner
